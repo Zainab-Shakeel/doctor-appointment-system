bef62b2672dd05a054a23c1e7b753f5676059d7f
Zainab-Shakeel <zainabshakeel400@gmail.com>
2025-06-16 16:36:09 +0500

    Added basic layout for patient dashboard with doctor list and appointment form

--- doctor-appointment-system/src/pages/PatientDashboard.jsx
+++ doctor-appointment-system/src/pages/PatientDashboard.jsx
@@ -1,5 +1,40 @@
 function PatientDashboard() {
-  return <h2>Patient Dashboard</h2>;
+    return (
+        <div style={{ padding: "2rem" }}>
+            <h2>Welcome, Patient</h2>
+
+            <div style={{ marginTop: "2rem" }}>
+                <h3>Available Doctors</h3>
+                <ul>
+                    <li>Dr. Ahmad (10 min)</li>
+                    <li>Dr. Sara (5 min)</li>
+                </ul>
+            </div>
+
+            <div style={{ marginTop: "2rem" }}>
+                <h3>Book Appointment</h3>
+
+                <form>
+                    <div>
+                        <label>Select Doctor: </label>
+                        <select>
+                            <option>Dr. Ahmad</option>
+                            <option>Dr. Sara</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label>Select Date:</label>
+                        <input type="date" />
+                    </div>
+
+                    <br />
+                    <button type="submit">Book</button>
+                </form>
+
+            </div>
+        </div>
+    )
 }
 
 export default PatientDashboard;
